8acac1d45c2859d74b0c6f45c8eccfba29a6c7a3
HaydarJohn <79381644+HaydarJohn@users.noreply.github.com>
2023-04-08 14:27:44 +0300

    Implemented Running and Jumping

--- Assets/_Game/Scripts/Charachter.cs
+++ Assets/_Game/Scripts/Charachter.cs
@@ -6,13 +6,18 @@ using UnityEngine;
 public class Charachter : MonoBehaviour
 {
     private CharacterController characterController;
+    private Rigidbody rigidbody;
     public Camera camera;
-    public float speed;
+    public float moveSpeed = 3;
+    private float speed;
+    public float jumpStrength;
+    public bool isGrounded;
    
 
     // Start is called before the first frame update
     void Start()
     {
+        rigidbody = GetComponent<Rigidbody>();
         characterController = GetComponent<CharacterController>();
     }
 
@@ -24,6 +29,14 @@ public class Charachter : MonoBehaviour
 
     void moveRelativeToCamera()
     {
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            speed = moveSpeed * 2;
+        }
+        else
+        {
+            speed = moveSpeed;
+        }
         float playerVertical = Input.GetAxis("Vertical");
         float playerHorizontal = Input.GetAxis("Horizontal");
         Vector3 forward = camera.transform.forward;
@@ -33,9 +46,30 @@ public class Charachter : MonoBehaviour
         forward = forward.normalized;
         right = right.normalized;
 
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rigidbody.AddForce(Vector3.up * jumpStrength, ForceMode.Impulse);
+            isGrounded = false;
+        }
+
         Vector3 move = forward * playerVertical + right * playerHorizontal;
-        characterController.Move(move * speed * Time.deltaTime);
+        transform.position += move * speed * Time.deltaTime;
     }
 
-    
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.transform.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.transform.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
 }
